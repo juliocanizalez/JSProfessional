8636ac7157f5f7e4ba90237655d60d5d129a3dc9
Julio Canizalez <juliocanizalez@outlook.com>
2020-11-08 23:52:50 -0600

    adding autoplay, mute/unmute

--- assets/js/MediaPlayer.js
+++ assets/js/MediaPlayer.js
@@ -1,16 +1,26 @@
 function MediaPlayer(config) {
     this.media = config.el
+    this.plugins = config.plugins || []
+
+    this._initPlugins()
 }
 
-MediaPlayer.prototype.play = () => this.media.play()
-MediaPlayer.prototype.pause = () => this.media.pause()
+MediaPlayer.prototype._initPlugins = function (){
+    this.plugins.forEach(plugin => {plugin.run(this)})
+}
 
-MediaPlayer.prototype.togglePlay = () =>{
-    if(this.media.pause()){
+MediaPlayer.prototype.play = function () {this.media.play()}
+MediaPlayer.prototype.pause = function() {this.media.pause()}
+
+MediaPlayer.prototype.togglePlay = function () {
+    if(this.media.paused){
         this.play()
     }else{
         this.pause()
     }
 }
+
+MediaPlayer.prototype.mute = function () {this.media.muted = true}
+MediaPlayer.prototype.unmute = function() {this.media.muted = false}
 
 export default MediaPlayer
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
-import MediaPlayer from '/assets/js/MediaPlayer.js'
-const video = document.querySelector("video")
-const button = document.querySelector("button")
-const player = new MediaPlayer({el: video})
-button.onclick = () => player.togglePlay()
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from '../plugins/AutoPlay.js'
+
+const video = document.querySelector('video')
+const player = new MediaPlayer({el: video, plugins: [
+    new AutoPlay()
+    ]})
+
+const playButton = document.querySelector('#playButton')
+playButton.onclick = () => player.togglePlay()
+
+const muteButton = document.querySelector('#muteButton')
+muteButton.onclick = () => {
+    if(player.media.muted){
+        player.unmute()
+    }else{
+        player.mute()
+    }
+}
